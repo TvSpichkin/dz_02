081ef2704e1bb89a62711459d82a137a88bf2e7a
TvSpichkin <ericlen@mail.ru>
2024-06-24 15:48:20 +0300

    Завершение adminMiddleware

--- src/globalMiddlewares/adminMiddleware.ts
+++ src/globalMiddlewares/adminMiddleware.ts
@@ -1,15 +1,23 @@
 import {Request, Response, NextFunction} from "express";
+import {SET} from "../settings";
 
+
+function fromUTF8ToBase64(code: string) {
+    return btoa(unescape(encodeURIComponent("admin:qwerty")));
+} // Кодирование строки в base64
 
 export function adminMiddleware(req: Request, res: Response, next: NextFunction) {
     const auth = req.headers["authorization"];
 
     function unauth() {
         res.sendStatus(401);
+    } // Отправка статуса ошибки о попытке несанкционированного доступа
+
+    if(!auth || (auth.slice(0, 6) !== "Basic ")) unauth(); // Проверка на базовую авторизацию
+    else {
+        const codedAuth = fromUTF8ToBase64(SET.ADMIN); // Получение base64 строки авторизации
+
+        if(auth.slice(6) !== codedAuth) unauth(); // Сравнение строк base64
+        else next(); // Передача управления дальше
     }
-
-    if(!auth || (auth.slice(0, 6) !== "Basic ")) unauth();
-    else if(auth.slice(0, 6) !== "Basic ") res.sendStatus(401);
-
-    next();
 }
