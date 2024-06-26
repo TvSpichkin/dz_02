719c77b5304ad5c0613e787d5d4943b86ce132ac
TvSpichkin <ericlen@mail.ru>
2024-06-26 12:00:46 +0300

    Типизация запросов

--- src/features/blogs/controllers/findBlogController.ts
+++ src/features/blogs/controllers/findBlogController.ts
@@ -1,9 +1,9 @@
-import {Request, Response} from "express";
+import {Response} from "express";
 import {BlogViewModel} from "../../../IOtypes/blogsTypes";
-import {blogsRep} from "../blogsRep";
+import {ReqParam} from "../../../IOtypes/reqTypes";
 
 
-export function findBlogController(req: Request<{id: string}>, res: Response<BlogViewModel>) {
+export function findBlogController(req: ReqParam<{id: string}>, res: Response<BlogViewModel>) {
     // @ts-ignore
     res.json(res.find); // Получение искомого сетевого журнала
 }; // Контролёр, отвечающий за выдачу искомого сетевого журнала
