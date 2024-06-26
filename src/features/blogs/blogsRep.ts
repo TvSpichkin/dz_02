c12095e6541e5c2e25cabac4d803c84a89e4f8a7
TvSpichkin <ericlen@mail.ru>
2024-06-26 12:32:18 +0300

    Контролёр, отвечающий за создание и возврат сетевого журнала

--- src/features/blogs/blogsRep.ts
+++ src/features/blogs/blogsRep.ts
@@ -6,10 +6,22 @@ import {BlogInputModel, BlogViewModel} from "../../IOtypes/blogsTypes";
 export const blogsRep = {
     getAll() {
         return db.blogs.map(this.maper);
-    }, // Извлечение сетевых журналов
+    }, // Извлечение всех сетевых журналов
     find(id: string) {
         return db.blogs.find(b => String(b.id) === id);
-    },
+    }, // Извлечение сетевого журнала по идентификатору
+    create(blog: BlogInputModel) {
+        const newBlog: BlogDbType = {
+            id: db.blogs.length ? db.blogs[db.blogs.length - 1].id + 1 : 1,
+            name: blog.name,
+            description: blog.description,
+            websiteUrl: blog.websiteUrl,
+        };
+
+        db.blogs.push(newBlog);
+
+        return this.maper(newBlog);
+    }, // Запись сетевого журнала в БД
     maper(blog: BlogDbType) {
         const blogForOutput: BlogViewModel = {
             id: String(blog.id),
