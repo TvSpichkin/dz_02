49a95910bc27978b93cb265fe6598ead8b8de50f
TvSpichkin <ericlen@mail.ru>
2024-07-06 21:07:26 +0300

    Покрытие тестами всех записей

--- __tests__/helpers/test-helpers.ts
+++ __tests__/helpers/test-helpers.ts
@@ -4,4 +4,5 @@ import {SET} from "../../src/settings";
 
 
 export const req = agent(app), // Определение запроса для тестирования программы
-    getBlog = req.get(SET.PATH.BLOGS); // Запрос на получение всех сетевых журналов
+    getBlog = req.get(SET.PATH.BLOGS), // Запрос на получение всех сетевых журналов
+    getPost = req.get(SET.PATH.POSTS); // Запрос на получение всех записей
